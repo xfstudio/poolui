e8b4340e99f07076b507721e10cd08a41ebaadae
windy <xfstudio@qq.com>
2017-12-18 02:00:46 +0800

    modify default config

--- app/globals.default.js
+++ app/globals.default.js
@@ -4,8 +4,8 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "XMRPool.net",
-		api_url : 'https://api.xmrpool.net',
+		pool_name: "pool.cdxfkj.cn",
+		api_url : 'https://api.cdxfkj.cn',
 		api_refresh_interval: 5000,
 		app_update_interval: 30*60000
 	};
